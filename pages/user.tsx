de5e7e2ad40a0ea645dd40519fc31929081e2856
Sanshiroh SAKAI <sansyo@gmail.com>
2025-07-23 06:58:41 +0900

    Update: ページ名整理

---
+++ pages/user.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "@/components/Layout";
+
+export default function UserPage() {
+  return (
+    <Layout>
+      <div className="min-h-screen bg-gray-50">User</div>
+    </Layout>
+  );
+}
